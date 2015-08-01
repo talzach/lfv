6d7dbc665c5df0d2b1d05dd455e4822cc3e0f4bf
talzach <zachtal@gmail.com>
2015-08-01 15:49:24 +0300

    new

--- public/js/services/QuestionService.js
+++ public/js/services/QuestionService.js
@@ -1,8 +1,13 @@
-angular.module('QuestionService', []).factory('QuestionService', ['$http', function($http) {
+angular.module('questionService', []).factory('questionService', ['$http', function ($http) {
 
     return {
-        getAsync : function() {
-            return $http.get('/api/questions/10').then(function(response) {
+        getAsync: function () {
+            return $http.get('/api/questions/10').then(function (response) {
+                return response.data;
+            });
+        },
+        saveAsync: function () {
+            return $http.get('/api/questions/10').then(function (response) {
                 return response.data;
             });
         }
