f7bd6c953b35a8f1a7bc2c4421d477f9d65d1006
talzach <zachtal@gmail.com>
2015-09-14 13:31:54 +0300

    graphic design

--- app/models/possibleAnswerModel.js
+++ app/models/possibleAnswerModel.js
@@ -3,12 +3,13 @@ var mongoose = require('mongoose'),
     autoIncrement = require('mongoose-auto-increment');
 
 var possibleAnswerSchema = new Schema({
+    _id: { type: Schema.Types.ObjectId },
     number: Number,
     text: String,
     restrictedQuestions: [{ type: Schema.Types.ObjectId, ref: 'Question' }]
 });
 
-possibleAnswerSchema.plugin(autoIncrement.plugin, {model: 'PossibleAnswer', field: 'number'});
+possibleAnswerSchema.plugin(autoIncrement.plugin, { model: 'PossibleAnswer', field: 'number' });
 module.exports = {
     model : mongoose.model('PossibleAnswer', possibleAnswerSchema),
     schema : possibleAnswerSchema
